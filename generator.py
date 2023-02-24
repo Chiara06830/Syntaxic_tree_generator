8d815034c36c2022cdeb4e95ee9d80507f38ff28
Chiara <chiara.relevat@outlook.fr>
2023-02-24 16:01:51 +0100

    final result

--- generator.py
+++ generator.py
@@ -2,15 +2,26 @@ import nltk
 from nltk.tokenize import word_tokenize
 nltk.download('universal_tagset')
 
+# ----------------------- DEFINE A GRAMMAR ----------------------- 
 grammar = r'''
-    NP: {<DET>?<ADJ>*<NOUN>}
-    VP: {<VERB.*>+<ADV>?}
+    NP: {<DET>?<ADJ>*<NOUN>} # Noun phrase
+    VP: {<VERB.*>+<ADV>?} # Verb phrase
 '''
 
-sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
-tagged = nltk.pos_tag(word_tokenize(sentence), tagset='universal')
+# ----------------------- DRAWING A TREE ----------------------- 
+def draw_syntactic_tree(sentence):
+    # tag the sentence with universal tokenizer (it's more general and readable)
+    tagged = nltk.pos_tag(word_tokenize(sentence), tagset='universal')
+    # create a parser based on the grammar
+    cp = nltk.RegexpParser(grammar)
+    # create a tree parsing the tag sentence with the parser
+    result = cp.parse(tagged)
+    # draw the tree
+    result.draw()
 
-cp = nltk.RegexpParser(grammar)
-result = cp.parse(tagged)
+# ----------------------- TESTING ----------------------- 
+draw_syntactic_tree("""At eight o'clock on Thursday morning Arthur didn't feel very good.""")
+draw_syntactic_tree("""I am waking up.""")
+draw_syntactic_tree("""Mike is working at the trainstation.""")
 
-result.draw()
+draw_syntactic_tree("""Sweet are the uses of adversity which, like the toad, ugly and venomous, wears yet a precious jewel in his head.""")
